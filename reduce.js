a93bc826e6135f80e436b8d7c1beabc4b1a38b27
Dylan <dylflanagan@gmail.com>
2023-01-18 14:28:19 -0800

    First reduce test passing

--- reduce.js
+++ reduce.js
@@ -2,7 +2,15 @@
 // OUTPUT: an array of all the sneakers brands
 // REQS: use .reduce
 
-export const getSneaks = (brands) => {};
+export const getSneaks = (brands) => {
+  //   let sneaks = [];
+  //   for (let brand of brands) {
+  //     sneaks = [...sneaks, ...brand.shoes];
+  //     console.log('sneaks at end of loop', sneaks);
+  //   }
+  //   return sneaks;
+  return brands.reduce((acc, brand) => [...acc, ...brand.shoes], []);
+};
 
 // INPUT: the array of dogs from data.js
 // OUTPUT: the average age of the dogs
